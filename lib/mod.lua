37d6a9d0f12a725d75c85497a28e2a7e7e615c3e
Naomi Seyfer <naomi@seyfer.org>
2023-10-15 23:03:19 +0100

    Better system for post-init hooks

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,5 +1,36 @@
 local fx = require("fx/lib/fx")
 local mod = require 'core/mods'
+local hook = require 'core/hook'
+local tab = require 'tabutil'
+-- Begin post-init hack block
+if hook.script_post_init == nil and mod.hook.patched == nil then
+    mod.hook.patched = true
+    local old_register = mod.hook.register
+    local post_init_hooks = {}
+    mod.hook.register = function(h, name, f)
+        if h == "script_post_init" then
+            post_init_hooks[name] = f
+        else
+            old_register(h, name, f)
+        end
+    end
+    mod.hook.register('script_pre_init', '!replace init for fake post init', function()
+        local old_init = init
+        init = function()
+            old_init()
+            for i, k in ipairs(tab.sort(post_init_hooks)) do
+                local cb = post_init_hooks[k]
+                print('calling: ', k)
+                local ok, error = pcall(cb)
+                if not ok then
+                    print('hook: ' .. k .. ' failed, error: ' .. error)
+                end
+            end
+        end
+    end)
+end
+-- end post-init hack block
+
 
 local FxDelay = fx:new{
     subpath = "/fx_delay"
@@ -15,8 +46,8 @@ function FxDelay:add_params()
     FxDelay:add_control("fx_delay_pingpong", "pingpong", "pingpong", controlspec.UNIPOLAR)
 end
 
-mod.hook.register("script_pre_init", "delay mod pre init", function()
-    FxDelay:install()
+mod.hook.register("script_post_init", "fx delay mod post init", function()
+    FxDelay:add_params()
 end)
 
 mod.hook.register("script_post_cleanup", "delay mod post cleanup", function()
